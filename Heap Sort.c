b7261ce725e69b79595438341372d33b4c38a047
Mukheem Mohammed <49473863+Mukheem1603@users.noreply.github.com>
2019-10-07 20:03:01 +0530

    Update Heap Sort.c

--- Heap Sort.c
+++ Heap Sort.c
@@ -1,68 +1,55 @@
-/*  Below program is written in C++ language  */
-
-#include <iostream>
-
-using namespace std;
-
-void heapify(int arr[], int n, int i)
+#include <stdio.h>
+ 
+void main()
 {
-    int largest = i;
-    int l = 2*i + 1;
-    int r = 2*i + 2;
+    int heap[10], no, i, j, c, root, temp;
  
-    // if left child is larger than root
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
- 
-    // if right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
- 
-    // if largest is not root
-    if (largest != i)
+    printf("\n Enter no of elements :");
+    scanf("%d", &no);
+    printf("\n Enter the nos : ");
+    for (i = 0; i < no; i++)
+       scanf("%d", &heap[i]);
+    for (i = 1; i < no; i++)
     {
-        swap(arr[i], arr[largest]);
- 
-        // recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
+        c = i;
+        do
+        {
+            root = (c - 1) / 2;             
+            if (heap[root] < heap[c])   /* to create MAX heap array */
+            {
+                temp = heap[root];
+                heap[root] = heap[c];
+                heap[c] = temp;
+            }
+            c = root;
+        } while (c != 0);
     }
-}
-
-void heapSort(int arr[], int n)
-{
-    // build heap (rearrange array)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
  
-    // one by one extract an element from heap
-    for (int i=n-1; i>=0; i--)
+    printf("Heap array : ");
+    for (i = 0; i < no; i++)
+        printf("%d\t ", heap[i]);
+    for (j = no - 1; j >= 0; j--)
     {
-        // move current root to end
-        swap(arr[0], arr[i]);
- 
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
-}
- 
-/* function to print array of size n */
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << "\n";
-}
- 
-
-int main()
-{
-    int arr[] = {121, 10, 130, 57, 36, 17};
-    int n = sizeof(arr)/sizeof(arr[0]);
- 
-    heapSort(arr, n);
- 
-    cout << "Sorted array is \n";
-    printArray(arr, n);
-}
+        temp = heap[0];
+        heap[0] = heap[j    /* swap max element with rightmost leaf element */
+        heap[j] = temp;
+        root = 0;
+        do 
+        {
+            c = 2 * root + 1;    /* left node of root element */
+            if ((heap[c] < heap[c + 1]) && c < j-1)
+                c++;
+            if (heap[root]<heap[c] && c<j)    /* again rearrange to max heap array */
+            {
+                temp = heap[root];
+                heap[root] = heap[c];
+                heap[c] = temp;
+            }
+            root = c;
+        } while (c < j);
+    } 
+    printf("\n The sorted array is : ");
+    for (i = 0; i < no; i++)
+       printf("\t %d", heap[i]);
+    printf("\n Complexity : \n Best case = Avg case = Worst case = O(n logn) \n");
+ }
